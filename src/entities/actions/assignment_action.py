e56cefa2e75fb898bcd5ba3872fcf63c8774089c
Raiyaan Rahman <raiyaan@modiface.com>
2022-11-28 09:25:34 -0500

    fix import errors in actions

--- src/entities/actions/assignment_action.py
+++ src/entities/actions/assignment_action.py
@@ -1,11 +1,11 @@
-from action import Action
+from .action import Action
 from ...utils import Utils
 
 class AssignmentAction(Action):
-    def __init__(self, number, you_or_thou='thou', punctuation='!',) -> None:
+    def __init__(self, expr, you_or_thou='thou', punctuation='!',) -> None:
         requirements = {'you_or_thou': you_or_thou}
         Utils.check_requirements(requirements)
 
         you = 'You' if you_or_thou == 'you' else 'Thou'
         are = 'are' if you_or_thou == 'you' else 'art'
-        super().__init__(f"{you} {are} a {str(number)}", punctuation)
+        super().__init__(f"{you} {are} {str(expr)}", punctuation)
