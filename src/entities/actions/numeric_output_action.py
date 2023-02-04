652fa5cc136909be0a028775e09e0bcbf3cae2a6
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2023-02-04 15:50:23 -0500

    implement numeric_output_action.py

--- src/entities/actions/numeric_output_action.py
+++ src/entities/actions/numeric_output_action.py
@@ -1,10 +1,10 @@
 from .action import Action
 from ...utils import Utils
 
-class StringOutputAction(Action):
+class NumericOutputAction(Action):
     def __init__(self, you_or_thou='you', punctuation='!') -> None:
         requirements = {'you_or_thou': you_or_thou}
         Utils.check_requirements(requirements)
         your_or_thy = 'your' if you_or_thou == 'you' else 'thy'
         
-        super().__init__(f'Speak {your_or_thy} mind', punctuation)
+        super().__init__(f'Open {your_or_thy} heart', punctuation)
